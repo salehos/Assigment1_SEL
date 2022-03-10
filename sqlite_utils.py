07b62c1ea0fe38e11342437de10d317227871454
amirmosio <amir.mn.mosio@gmail.com>
2022-03-10 18:54:15 +0330

    Implement increment_user_usage method to save user usages

--- sqlite_utils.py
+++ sqlite_utils.py
@@ -5,16 +5,32 @@ class SqliteQueryUtils:
     database_path = 'test.db'
 
     def __init__(self):
-        self._create_user_history_database()
-
-    def _execute_query(self, query_string):
-        conn = sqlite3.connect(self.database_path)
-        conn.execute(query_string)
-        conn.close()
+        try:
+            self._create_user_history_database()
+        except:
+            pass
 
     def _create_user_history_database(self):
-        self._execute_query("""
-        CREATE TABLE USER_HISTORY
-        (ID INT PRIMARY KEY NOT NULL,
-        USERNAME TEXT NOT NULL,
-        USAGE INT DEFAULT 0 NOT NULL""")
+        with sqlite3.connect(self.database_path) as conn:
+            conn.execute("""
+            CREATE TABLE USER_HISTORY
+            (username TEXT PRIMARY KEY  NOT NULL,
+            usage INT DEFAULT 0 NOT NULL)""")
+
+    def increment_user_usage(self, username):
+        with sqlite3.connect(self.database_path) as conn:
+            user = conn.execute(f"""SELECT * from USER_HISTORY where username='{username}'""")
+            user = list(user)
+            if len(user) == 1:
+                usage = user[0][1]
+                conn.execute(f"""UPDATE USER_HISTORY set usage={usage + 1} WHERE username='{username}'""")
+            elif len(user) == 0:
+                conn.execute(f"""INSERT INTO USER_HISTORY (username, usage) \
+                      VALUES ('{username}', {1})""")
+            else:
+                raise Exception("Multiple user found with this username!")
+
+
+if __name__ == '__main__':
+    database = SqliteQueryUtils()
+    database.increment_user_usage("hello")
